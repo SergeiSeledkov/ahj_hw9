6021943a756c87988e701849772682bd43a305d3
localpc123 <gthemail92@gmail.com>
2022-06-07 00:44:48 +0500

    v2

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,13 @@
 const collapseButton = document.querySelector('.collapse');
 const wrapper = document.querySelector('.wrapper');
+const collapseText = document.querySelector('.collapse-text');
 
 collapseButton.addEventListener('click', () => {
   wrapper.classList.toggle('open');
+
+  if (wrapper.classList.contains('open')) {
+    wrapper.style.height = `${collapseText.offsetHeight}px`;
+  } else {
+    wrapper.style.height = 0;
+  }
 });
